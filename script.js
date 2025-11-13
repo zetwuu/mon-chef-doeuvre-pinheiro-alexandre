48a3e8a5e2c327c58a7310ef1f014004284900fd
ZetWu <63952346+zetwuu@users.noreply.github.com>
2025-11-13 14:17:42 +0000

    feat: Ajout de script.js et première interaction DOM

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("Le script est chargé !");
+
+const titre = document.querySelector('#titre-principal');
+
+if (titre) {
+
+  titre.addEventListener('click', () => {
+
+    console.log("J'ai cliqué sur le titre !");
+    titre.textContent = "Titre cliqué !";
+  });
+}
